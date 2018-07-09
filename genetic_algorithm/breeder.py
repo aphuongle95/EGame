34fc23ef812eb0011470d3e7c03f25f5eff9b99a
le anh phuong <annie.le@bizedge.com>
2018-07-09 10:57:45 +0200

    init function

--- genetic_algorithm/breeder.py
+++ genetic_algorithm/breeder.py
@@ -3,6 +3,7 @@ from game.individuals.dot import Dot
 from random import choice, uniform
 from copy import copy
 
+import random as random
 import numpy as np
 
 class Breeder:
@@ -15,8 +16,38 @@ class Breeder:
         it gets a population consisting of individuals
         each individual has certain statistics and traits
         """
+        population_cpy = copy(population)
+        dead = []
+        alive = []
+        for individual in population_cpy:
+            if individual.dead:
+                dead.append(individual)
+            else:
+                alive.append(individual)
+
+        for _ in range(len(dead)):
+            # get the position where the child should be inserted on the field
+            where = choice(alive)._position
+            color = alive[0].color
+
+            selected = self.select_example(population_cpy)
+            parent1 = selected[0]
+            parent2 = selected[1]
+            child1, child2 = self.crossover_example(copy(parent1), copy(parent2))
+            child1 = self.tweak_example(child1)
+            child2 = self.tweak_example(child2)
+            score_child1 = self.assess_individual_fitness_example(child1)
+            score_child2 = self.assess_individual_fitness_example(child2)
+            if score_child1 > score_child2:
+                new_individual = Dot(self.parent, color=color, position=where, dna=child1.get_dna())
+            else:
+                new_individual = Dot(self.parent, color=color, position=where, dna=child2.get_dna())
+            population_cpy.append(new_individual)
+        for dead_individual in dead:
+            population_cpy.remove(dead_individual)
+        return population_cpy
         # return self.breed_copy_dead_example(population)
-        return self.breed_example_with_ga(population)
+        # return self.breed_example_with_ga(population)
 
     def initialize_population(self, num_individuals, color):
         """
@@ -24,9 +55,67 @@ class Breeder:
         it gets the number of individuals which have to be generated
         also, it gets the color of the population
         """
-        return self.initialize_population_example(num_individuals, color)
+        population = []
+        for i in range(num_individuals):
+            greater_than = 0.3
+            less_than = 0.5
+            centric_a = round(random.uniform(greater_than, less_than), 2)
+            centric_b = round(random.uniform(greater_than, less_than), 2)
+            centric_c = round(random.uniform(greater_than, less_than), 2)
+            a = np.random.dirichlet(np.ones(5), size=1)[0] * (1-centric_a)#perception
+            b = np.random.dirichlet(np.ones(4), size=1)[0] * (1-centric_b)#ability
+            c = np.random.dirichlet(np.ones(5), size=1)[0] * (1-centric_c)#desire
+            if(i%5==0):
+                a = np.insert(a, 4, centric_a)
+                b = np.insert(b, 4, centric_b)
+                c = np.insert(c, 0, centric_c)
+            elif(i%5==1):
+                a = np.insert(a, 5, centric_a)
+                b = np.insert(b, 0, centric_b)
+                c = np.insert(c, 1, centric_c)
+            elif(i%5==2):
+                a = np.insert(a, 0, centric_a)
+                b = np.insert(b, 1, centric_b)
+                c = np.insert(c, 2, centric_c)
+            elif(i%5==3):
+                a = np.insert(a, 1, centric_a)
+                b = np.insert(b, 2, centric_b)
+                c = np.insert(c, 4, centric_c)
+            elif(i%5==4):
+                a = np.insert(a, 2, centric_a)
+                b = np.insert(b, 3, centric_b)
+                c = np.insert(c, 5, centric_c)
+            else:
+                a = np.random.dirichlet(np.ones(5), size=1)[0]
+                b = np.random.dirichlet(np.ones(5), size=1)[0]
+                c = np.random.dirichlet(np.ones(5), size=1)[0]
+            population.append(self.assign_value(a, b, c, color))
+        return population
+        # return self.initialize_population_example(num_individuals, color)
 
-    
+    def assign_value(self, a, b, c, color):
+        x = Dot(self.parent, color=color)
+        x.perception.predator = a[0]
+        x.perception.poison = a[1]
+        x.perception.health_potion = a[2]
+        x.perception.opponent = a[3] #
+        x.perception.food = a[4]
+        x.perception.corpse = a[5]
+
+        x.abilities.armor_ability = b[0]
+        x.abilities.speed = b[1]
+        x.abilities.strength = b[2]
+        x.abilities.poison_resistance = b[3]
+        x.abilities.toxicity = b[4]
+
+        x.desires.dodge_predators = c[0]
+        x.desires.dodge_poison = c[1]
+        x.desires.seek_potion = c[2]
+        x.desires.seek_opponents = c[3] #
+        x.desires.seek_food = c[4]
+        x.desires.seek_corpse = c[5]
+        return x
+
     def initialize_population_example(self, num_individuals, color):
         """
         example initializer
@@ -174,11 +263,11 @@ class Breeder:
         for i in range(len(population)):
             score = self.assess_individual_fitness_example(population[i])
             fitness_array[i] = score
-        
+
         # span value range
         for i in range(1, len(fitness_array)):
             fitness_array[i] = fitness_array[i] + fitness_array[i - 1]
-        
+
         parents = self.selectParentSUS(population, fitness_array, 2)
         return parents
 
